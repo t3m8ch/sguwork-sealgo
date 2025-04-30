19691ee211f29077092148470798a5ed2c00ebf8
Artem Kudyakov <t3m8ch@yandex.ru>
2025-04-30 15:44:18 +0400

    wip: Make null-indexed sorting

--- practice/heap-sort/heapsort.py
+++ practice/heap-sort/heapsort.py
@@ -1,9 +1,11 @@
 import random
+
+
 def sift_down(a, v, n):
-    while 2*v <= n:
-        u = 2*v
-        if 2*v+1 <= n and a[2*v+1] < a[2*v]:
-            u = 2*v+1
+    while 2*v+1 < n:
+        u = 2*v+1
+        if 2*v+2 < n and a[2*v+2] < a[2*v+1]:
+            u = 2*v+2
         if a[u] < a[v]:
             a[u], a[v] = a[v], a[u]
             v = u
@@ -13,7 +15,7 @@ def sift_down(a, v, n):
 
 def heapify(a, n):
     # n..=1
-    for i in range(n, 0, -1):
+    for i in range(n-1, -1, -1):
         sift_down(a, i, n)
 
 
@@ -26,13 +28,13 @@ def sort(a, n):
     for i in range(1, n+1):
         print(f"Step {i}: {a}")
         # a[1] = a[n]
-        a[1], a[n] = a[n], a[1]
+        a[0], a[n-1] = a[n-1], a[0]
         n -= 1
-        sift_down(a, 1, n)
+        sift_down(a, 0, n)
 
 
 n = 100
-a = [0] + [random.randint(0, 10000) for _ in range(n)]
+a = [random.randint(0, 10000) for _ in range(n)]
 
 sort(a, n)
 print(a)
